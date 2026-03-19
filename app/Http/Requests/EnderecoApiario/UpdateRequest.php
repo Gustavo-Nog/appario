871ac87fb345dd7a2fb54e87f002500fa1e29b54
Gustavo-Nog <gustavopan3g7@gmail.com>
2026-03-18 21:23:28 -0300

    feat (endereco): implementa lógica da model e formRequests para apiario

---
+++ app/Http/Requests/EnderecoApiario/UpdateRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\EnderecoApiario;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $ufs = [
+            'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES',
+            'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR',
+            'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC',
+            'SP', 'SE', 'TO'
+        ];
+
+        return [
+            'logradouro' => 'sometimes|string|max:80',
+            'numero' => 'sometimes|string|max:10',
+            'complemento' => 'nullable|string|max:75',
+            'bairro' => 'sometimes|string|max:50',
+            'cep' => 'sometimes|string|size:10',
+            'cidade' => 'sometimes|string|max:50',
+            'estado' => ['sometimes', 'string', 'size:2', Rule::in($ufs)],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'cep.size' => 'O CEP deve conter exatamente 10 caracteres.',
+            'estado.in' => 'Selecione um estado válido.',
+            'numero.max' => 'Número excede o tamanho máximo de :max caracteres.',
+        ];
+    }
+}
