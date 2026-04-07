7f13239b8ba2819498f0aa159831b190eebc7bca
Gustavo-Nog <gustavopan3g7@gmail.com>
2026-04-06 21:25:20 -0300

    feat: inicia refatoração da view e de regras do css

--- resources/views/apiarios/mostrar.blade.php
+++ resources/views/apiarios/mostrar.blade.php
@@ -1,32 +1,74 @@
 @extends('layouts.app') {{-- ou o layout que você estiver usando --}}
-
+@section('title', 'Detalhes do Apiário')
+@push('styles')
+<link href="{{ asset('css/apiarios/mostrar.css') }}" rel="stylesheet" />
+@endpush
 @section('content')
-<div class="container mt-4">
-    <h1 class="mb-4">Detalhes do Apiário</h1>
+<div class="container mt-2">
 
-    <div class="card p-4 mb-3">
-        <h3>Informações Gerais</h3>
-        <p><strong>Nome:</strong> {{ $apiario->nome }}</p>
-        <p><strong>Data de Criação:</strong> {{ \Carbon\Carbon::parse($apiario->data_criacao)->format('d/m/Y') }}</p>
-        <p><strong>Área:</strong> {{ $apiario->area }} hectares</p>
-        <p><strong>Coordenadas:</strong> {{ $apiario->coordenadas ?? 'Não informado' }}</p>
+    <div class="card p-3 mb-2">
+        <h3 class="card-title d-flex justify-content-center pb-3">Informações Gerais</h3>
+        <div class="row">
+            <div class="col-6 container-apiario">
+                <div class="informacoes-gerais">
+                    <p class="info-apiario">Nome do Apiário: </p> <p class="info-value-apiario">{{ $apiario->nome }}</p>
+                </div>
+                <div class="informacoes-gerais">
+                    <p class="info-apiario">Criação: </p> <p class="info-value-apiario">{{ \Carbon\Carbon::parse($apiario->data_criacao)->format('d/m/Y') }}</p>            
+                </div>
+                <div class="informacoes-gerais">
+                    <p class="info-apiario">Área: </p> <p class="info-value-apiario">{{ $apiario->area }}</p>
+                </div>
+                <div class="informacoes-gerais">
+                    <p class="info-apiario">Coordenadas: </p> <p class="info-value-apiario">{{ $apiario->coordenadas ?? 'Não informado' }}</p>
+                </div>
+            </div>
+            <div class="col-6 container-colmeia">
+                <div class="informacoes-colmeias">
+                    <p>Total de Colmeias</p>
+                </div>
+                 <h1 class="colmeias-count">
+                    {{ $apiario->colmeias->count() }}
+                </h1>
+            </div>
+        </div>
+        
+        
     </div>
 
-    <div class="card p-4">
-        <h3>Endereço</h3>
+    <div class="card p-3 mb-2">
+        <h3 class="card-title d-flex justify-content-center">Endereço</h3>
         @if($apiario->endereco)
-            <p><strong>Estado (UF):</strong> {{ $apiario->endereco->estado }}</p>
-            <p><strong>Cidade:</strong> {{ $apiario->endereco->cidade }}</p>
-            <p><strong>Logradouro:</strong> {{ $apiario->endereco->logradouro }}</p>
-            <p><strong>Número:</strong> {{ $apiario->endereco->numero }}</p>
-            <p><strong>Complemento:</strong> {{ $apiario->endereco->complemento ?? '---' }}</p>
-            <p><strong>Bairro:</strong> {{ $apiario->endereco->bairro }}</p>
-            <p><strong>CEP:</strong> {{ $apiario->endereco->cep }}</p>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->estado }}</p>
+            </div>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->cidade }}</p>
+            </div>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->logradouro }}</p>
+            </div>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->numero }}</p>
+            </div>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->numero }}</p>
+            </div>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->complemento ?? '---' }}</p>
+            </div>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->bairro }}</p>
+            </div>
+            <div class="endereco">
+                <p> {{ $apiario->endereco->cep }}</p>
+            </div>
         @else
-            <p>Endereço não cadastrado.</p>
+            <p>Nenhum endereço cadastrado.</p>
         @endif
     </div>
 
     <a href="{{ route('apiarios.index') }}" class="btn btn-secondary mt-3">Voltar</a>
+
 </div>
 @endsection
