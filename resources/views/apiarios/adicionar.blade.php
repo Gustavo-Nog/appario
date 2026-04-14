f77d953807b69999c98076e26417fa8fef9df930
Gustavo-Nog <gustavopan3g7@gmail.com>
2026-04-13 21:43:04 -0300

    refactor: padronizar sequencia dos inputs e placeholders do formulario

--- resources/views/apiarios/adicionar.blade.php
+++ resources/views/apiarios/adicionar.blade.php
@@ -6,20 +6,19 @@
 
 @section('content')
         <div class="form-wrapper">
-          <form method="POST" action="{{ route('apiarios.store') }}" class="apiario-form">
+          <form method="POST" action="{{ route('apiarios.store') }}" class="apiario-form row">
             @csrf
-            <h1 class="text-center mb-4">Informe os dados do Apiário</h1>
+            <h1 class="text-center mb-4">Cadastrar Apiário</h1>
 
-            {{-- Nome --}}
-            <div>
+            <div class="col-md-6 col-12 field-nome-apiario">
               <label for="nome">Nome do Apiário<span style="color: red">*</span></label>
-              <input type="text" class="form-control @error('nome') is-invalid @enderror" name="nome" id="nome" value="{{ old('nome') }}" required />
+              <input type="text" class="form-control @error('nome') is-invalid @enderror" name="nome" id="nome" value="{{ old('nome') }}" required placeholder="Digite um nome"/>
               @error('nome')
                 <div class="invalid-feedback">{{ $message }}</div>
               @enderror
             </div>
 
-            <div class="mb-3">
+            <div class="col-md-6 col-12 field-data">
               <label for="data_criacao">Data de Criação<span style="color: red">*</span></label>
               <input type="date" class="form-control @error('data_criacao') is-invalid @enderror" id="data_criacao" name="data_criacao" value="{{ old('data_criacao') }}" required />
               @error('data_criacao')
@@ -27,8 +26,7 @@
               @enderror
             </div>
 
-            {{-- Área --}}
-            <div class="mb-3">
+            <div class="col-md-6 col-12 field-area">
               <label for="area">Área (hectare)<span style="color: red">*</span></label>
               <input type="number" step="0.01" class="form-control @error('area') is-invalid @enderror" id="area" name="area" value="{{ old('area') }}" required />
               @error('area')
@@ -36,8 +34,7 @@
               @enderror
             </div>
 
-            {{-- Coordenadas --}}
-            <div class="mb-3">
+            <div class="col-md-6 col-12 field-coordenadas">
               <label for="coordenadas">Coordenadas (GPS)</label>
               <input type="text" class="form-control @error('coordenadas') is-invalid @enderror" id="coordenadas" name="coordenadas" value="{{ old('coordenadas') }}" placeholder="Latitude, Longitude (ex: 00.0000, 00.0000)"/>
               @error('coordenadas')
@@ -45,11 +42,9 @@
               @enderror
             </div>
 
-            <h3>Localização</h3>
-            {{-- Estado --}}
-            <div>
+            <div class="col-md-6 col-12 field-estado">
               <label for="estado" class="form-label">Estado (UF)<span style="color: red">*</span></label>
-              <select class="form-select @error('estado') is-invalid @enderror" id="estado" name="estado" required>
+              <select class="form-control @error('estado') is-invalid @enderror" id="estado" name="estado" required>
                 <option value="">Selecione...</option>
                 @foreach($ufs as $sigla => $nome)
                   <option value="{{ $sigla }}" {{ old('estado') == $sigla ? 'selected' : '' }}>
@@ -62,53 +57,23 @@
               @enderror
             </div>
 
-            <!-- Cidade -->
-            <div class="mb-3">
-              <label for="cidade" class="form-label">Cidade<span style="color: red">*</span></label>
-              <input type="text" class="form-control @error('cidade') is-invalid @enderror" id="cidade" name="cidade" value="{{ old('cidade') }}" required />
-              @error('cidade')
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-            </div>
-
-            <!-- Logradouro -->
-            <div class="mb-3">
-              <label for="logradouro" class="form-label">Logradouro<span style="color: red">*</span></label>
-              <input type="text" class="form-control @error('logradouro') is-invalid @enderror" id="logradouro" name="logradouro" value="{{ old('logradouro') }}"  required/>
-              @error('logradouro')
-                <div style="color:red;">{{ $message }}</div>
-              @enderror      
-            </div>
-
-            {{-- Número --}}
-            <div class="mb-3">
-              <label for="numero">Número<span style="color: red">*</span></label>
-              <input type="text" class="form-control @error('numero') is-invalid @enderror" id="numero" name="numero" value="{{ old('numero') }}" required />
-              @error('numero')
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-            </div>
-
-            {{-- Complemento --}}
-            <div class="mb-3">
-              <label for="complemento">Complemento</label>
-              <input type="text" class="form-control @error('complemento') is-invalid @enderror" id="complemento" name="complemento" value="{{ old('complemento') }}" />
-              @error('complemento')
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-            </div>
-
-            {{-- Bairro --}}
-            <div class="mb-3">
+            <div class="col-md-6 col-12 field-bairro">
               <label for="bairro">Bairro<span style="color: red">*</span></label>
-              <input type="text" class="form-control @error('bairro') is-invalid @enderror" id="bairro" name="bairro" value="{{ old('bairro') }}" required />
+              <input type="text" class="form-control @error('bairro') is-invalid @enderror" id="bairro" name="bairro" value="{{ old('bairro') }}" required placeholder="Digite seu bairro"/>
               @error('bairro')
                 <div class="invalid-feedback">{{ $message }}</div>
               @enderror
             </div>
 
-            {{-- CEP --}}
-            <div class="mb-3">
+            <div class="col-md-6 col-12 field-numero">
+              <label for="numero">Número<span style="color: red">*</span></label>
+              <input type="text" class="form-control @error('numero') is-invalid @enderror" id="numero" name="numero" value="{{ old('numero') }}" required placeholder="Digite o número do endereço"/>
+              @error('numero')
+                <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+            </div>
+
+            <div class="col-md-6 col-12 field-cep">
               <label for="cep">CEP<span style="color: red">*</span></label>
               <input type="text" class="form-control @error('cep') is-invalid @enderror" id="cep" name="cep" value="{{ old('cep') }}" required maxlength="10" placeholder="00000-000"/>
               @error('cep')
@@ -116,7 +81,40 @@
               @enderror
             </div>
 
-            <button type="submit" class="button">Cadastrar</button>
+            <div class="col-md-6 col-12 field-cidade">
+              <label for="cidade" class="form-label">Cidade<span style="color: red">*</span></label>
+              <input type="text" class="form-control @error('cidade') is-invalid @enderror" id="cidade" name="cidade" value="{{ old('cidade') }}" required placeholder="Digite o nome da cidade"/>
+              @error('cidade')
+                <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+            </div>
+
+            <div class="col-md-6 col-12 field-logradouro">
+              <label for="logradouro" class="form-label">Logradouro<span style="color: red">*</span></label>
+              <input type="text" class="form-control @error('logradouro') is-invalid @enderror" id="logradouro" name="logradouro" value="{{ old('logradouro') }}"  required placeholder="Digite o logradouro"/>
+              @error('logradouro')
+                <div style="color:red;">{{ $message }}</div>
+              @enderror      
+            </div>
+
+            <div class="col-md-6 col-12 field-complemento">
+              <label for="complemento">Complemento</label>
+              <input type="text" class="form-control @error('complemento') is-invalid @enderror" id="complemento" name="complemento" value="{{ old('complemento') }}" />
+              @error('complemento')
+                <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+            </div>
+
+            <div class="col-12 mt-3 field-submit">
+              <div class="row">
+                <div class="col-md-6 col-12">
+                  <button type="button" class="button-cancelar" onclick="window.location='{{ route('apiarios.index') }}'">Cancelar</button>
+                </div>
+                <div class="col-md-6 col-12">
+                  <button type="submit" class="button">Cadastrar</button>
+                </div>
+              </div>
+            </div>
           </form>
         </div>
     @endsection
