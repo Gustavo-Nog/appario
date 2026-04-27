36706b9eb2b3c984591ad124a55a7b5f0158a98e
Gustavo-Nog <gustavopan3g7@gmail.com>
2026-04-27 19:39:00 -0300

    docs: adiciona tipagem nos métodos do repository

--- app/Repositories/ColmeiaRepository.php
+++ app/Repositories/ColmeiaRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\Colmeia;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
 
 class ColmeiaRepository
@@ -13,7 +14,7 @@ class ColmeiaRepository
         $this->colmeiaModel = $colmeiaModel;
     }
 
-    public function getColmeiasByApiario(int $apiario_id) 
+    public function getColmeiasByApiario(int $apiario_id): Collection
     {
         $colmeias = $this->colmeiaModel
             ->where('apiario_id', $apiario_id)
@@ -22,7 +23,7 @@ class ColmeiaRepository
         return $colmeias;
     }
 
-    public function getColmeiasByApiarios(array $apiarioIds)
+    public function getColmeiasByApiarios(array $apiarioIds): Collection
     {
         $colmeias = $this->colmeiaModel
             ->whereIn('apiario_id', $apiarioIds)
@@ -30,7 +31,7 @@ class ColmeiaRepository
         return $colmeias;
     }
 
-    public function findByIdApiario(int $id_colmeia, int $id_apiario)
+    public function findByIdApiario(int $id_colmeia, int $id_apiario): Colmeia
     {
         $colmeia = $this->colmeiaModel
             ->where('id_colmeia', $id_colmeia)
@@ -39,21 +40,21 @@ class ColmeiaRepository
         return $colmeia;
     }
 
-    public function createColmeia(int $id_apiario, array $data)
+    public function createColmeia(int $id_apiario, array $data): Colmeia
     {
         $data['apiario_id'] = $id_apiario;
         $colmeia = $this->colmeiaModel->create($data);
         return $colmeia;
     }
 
-    public function updateColmeia(int $id_colmeia, int $id_apiario, array $data)
+    public function updateColmeia(int $id_colmeia, int $id_apiario, array $data): Colmeia
     {
         $colmeia = $this->findByIdApiario($id_colmeia, $id_apiario);
         $colmeia->update($data);
         return $colmeia;
     }
 
-    public function deleteColmeia(int $id_colmeia, int $id_apiario)
+    public function deleteColmeia(int $id_colmeia, int $id_apiario): bool
     {
         $colmeia = $this->findByIdApiario($id_colmeia, $id_apiario);
         return (bool) $colmeia->delete();
